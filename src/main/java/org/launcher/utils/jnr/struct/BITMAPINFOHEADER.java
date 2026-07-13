40a5bd83eba2f493024845e63ba1a7c9eecc6cb7
egoru <dezertir.106245@gmail.com>
2026-07-13 17:56:44 +0500

    0.8a

--- src/main/java/org/launcher/utils/jnr/struct/BITMAPINFOHEADER.java
+++ src/main/java/org/launcher/utils/jnr/struct/BITMAPINFOHEADER.java
@@ -3,14 +3,17 @@ package org.launcher.utils.jnr.struct;
 import jnr.ffi.Struct;
 
 public class BITMAPINFOHEADER extends Struct {
-    public final Unsigned32 size = new Unsigned32();
-    public final Signed32 width = new Signed32();
-    public final Signed32 height = new Signed32();
-    public final Unsigned16 planes = new Unsigned16();
-    public final Unsigned16 bitCount = new Unsigned16();
-    public final Unsigned32 compression = new Unsigned32();
-
-    public final byte[] rest = new byte[20];
+    public final Unsigned32 biSize = new Unsigned32();
+    public final Signed32 biWidth = new Signed32();
+    public final Signed32 biHeight = new Signed32();
+    public final Unsigned16 biPlanes = new Unsigned16();
+    public final Unsigned16 biBitCount = new Unsigned16();
+    public final Unsigned32 biCompression = new Unsigned32();
+    public final Unsigned32 biSizeImage = new Unsigned32();
+    public final Signed32 biXPelsPerMeter = new Signed32();
+    public final Signed32 biYPelsPerMeter = new Signed32();
+    public final Unsigned32 biClrUsed = new Unsigned32();
+    public final Unsigned32 biClrImportant = new Unsigned32();
 
     public BITMAPINFOHEADER(jnr.ffi.Runtime runtime) {
         super(runtime);
